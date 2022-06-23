bde7065ac352977f8633ffeb3df383593e1c226f
Marylin  Alvarado <104533185+Marylin-Alvarado@users.noreply.github.com>
2022-06-23 10:31:13 -0500

    Add files via upload

---
+++ PateleriaProyecto Avance 2/src/model/Galleta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Edison
+ */
+public class Galleta extends Categoria{
+    
+    public String chispasDeChocolate;
+    public Integer limon;
+    public Integer naranja;
+    public Integer chocolate;
+    
+}
---
+++ PateleriaProyecto Avance 2/src/model/Item.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Edison
+ */
+public class Item {
+    
+    private String nombreItem;
+    private Integer unidad;
+    private Double precio;
+
+    public Item(String nombre) {
+        this.nombreItem = nombre;
+    }
+
+    public Item(Integer unidad) {
+        this.unidad = unidad;
+    }
+
+    public Item(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombreItem() {
+        return nombreItem;
+    }
+
+    public void setNombreItem(String nombre) {
+        this.nombreItem = nombre;
+    }
+
+    public Integer getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(Integer unidad) {
+        this.unidad = unidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "nombre=" + nombreItem + ", unidad=" + unidad + ", precio=" + precio + '}';
+    }
+
+    
+}
